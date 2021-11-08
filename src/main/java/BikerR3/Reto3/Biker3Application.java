888af2931225e2e85b7adc3c964258ca80a0da33
alejandro <kevin.corredor.mt@correo.usa.edu.co>
2021-11-08 13:52:49 -0500

    proyecto back final

---
+++ src/main/java/BikerR3/Reto3/Biker3Application.java
@@ -0,0 +1,20 @@
+// 1.Modelo o Entidad
+// 2.Interface
+// 3.Repositorio
+// 4.Servicios
+// 5.Controlador o Web
+
+
+package BikerR3.Reto3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Biker3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Biker3Application.class, args);
+	}
+
+}
---
+++ src/main/java/BikerR3/Reto3/InterfaceReservaciones.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package BikerR3.Reto3;
+
+import java.util.Date;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author JAMP
+ */
+public interface InterfaceReservaciones extends CrudRepository<Reservaciones,Integer>{
+     public List<Reservaciones>findAllByStatus(String status);
+
+    public List<Reservaciones>findAllByStartDateAfterAndStartDateBefore(Date dateOne,Date dateTwo);
+        
+    @Query("SELECT c.client, COUNT(c.client) FROM Reservaciones AS c GROUP BY c.client order by COUNT(c.client)DESC")
+
+    public List<Object[]> countTotalReservationsByClient();
+}
